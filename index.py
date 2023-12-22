d35184cb5cd230c7a3b3a611ad5aee30a3541d41
mohitsh7979 <mohitsh7979@gmail.com>
2023-12-22 20:01:59 +0530

    added

---
+++ index.py
@@ -0,0 +1,37 @@
+# a = 10   # int 
+# b = 1.56 # float
+# c = "Mohit"  # string 
+# d = False  # bool    
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# a = 10
+# b = 20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(4%2)
+# print(7%2)
+# print(8**2)
+# print(7//2)
+
+ 
+# first = "90"  # string
+# second = "90" # integer
+# print(type(first))
+# print(type(second))
+# print(type(first+second))
+
+
+_no = 5
+print(_no)
+
+no = 5
+print(_no)
+
+
